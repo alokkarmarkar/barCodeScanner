6a3f92e00eab74e71078a651ca40782f595921bf
alok karmarkar <alokkarmarkar12@gmail.com>
2022-12-21 19:04:25 +0530

    barcode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'scanner';
+
+  value: any;
+  isError = true;
+  openCamera : boolean =false
+
+  onError(error:any) {
+    console.error(error);
+    this.isError = true;
+  }
+  openscanner(){
+    this.openCamera=true
+  }
+  closeCamera(){
+    this.openCamera=false
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgxBarcodeScannerModule} from '@eisberg-labs/ngx-barcode-scanner';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environments';
+import { ResultComponent } from './result/result.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxBarcodeScannerModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
